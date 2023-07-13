f3f345e3937ca7cc65d89448e2190cd4db990e5e
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-07-13 12:36:38 -0300

    Oh yes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import './assets/main.css'
+import '@unocss/reset/tailwind.css'
+import 'virtual:uno.css'
 
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'
