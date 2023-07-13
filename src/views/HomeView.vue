e48c0d2138f308a54a36369b6905bb15002c1d1e
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-07-13 12:37:02 -0300

    Oh yes (#1)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="w-30 h-full bg-amber"></div>
 </template>
