55e69c2bdf6c10231593c87d504d212ca2d693fa
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-01-25 23:02:54 -0400

    Fantastic Vue 💞

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,19 @@ const handleClick = (event: MouseEvent) => {
 
   event.target?.dispatchEvent(new CustomEvent('click'))
 }
+
+const onFocusOut = (event: FocusEvent) => {
+  // 😱
+  console.log('focus out, related target:', event.relatedTarget)
+}
 </script>
 
 <template>
   <main>
-    <div>
+    <div @focusout="onFocusOut">
       <button @click="handleClick">Click me</button>
+      <input type="text" placeholder="Oh wow 1" />
+      <input type="text" placeholder="Oh wow 2" />
     </div>
   </main>
 </template>
